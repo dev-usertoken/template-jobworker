287d73450ef2b9df2dfd8e62da473a49e62278a7
wsbbb331 <tianyang@unvig.com>
2018-10-15 12:09:05 -0700

    update subproject

---
+++ configs/localconfigs.js
@@ -0,0 +1,10 @@
+// ///
+export const HOSTNAME = 'Usertoken SmartContract Manager';
+export const DATA_FILE = `data/data-${HOSTNAME}`;
+export const DEVICE_ID = false;
+
+// ///
+export const GRAPHQL = `https://${HOSTNAME}/api/graphql`;
+export const DBFILE = DATA_FILE;
+export const DEBUG = false;
+export const DEBUG_LOG_URL = 'https://logentries.com/app/a092e388';
